289bd70ada6436e31d5985d6669dd9a34938b0bd
Alexander Shelepenok <alxshelepenok@gmail.com>
2026-03-28 00:18:03 +0300

    feat: добавлен Dockerfile

--- server/index.js
+++ server/index.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const config = require('config');
-const path = require('path');
-const fileUpload = require('express-fileupload');
-const fs = require('fs');
-const useragent = require('express-useragent');
-const {slugify} = require('transliteration');
+const express = require("express");
+const config = require("config");
+const path = require("path");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const useragent = require("express-useragent");
+const {slugify} = require("transliteration");
 
 const app = express();
-app.use(express.json({limit: '10mb'}));
+app.use(express.json({limit: "10mb"}));
 app.use(useragent.express());
 
-app.get('/', async (req, res) => {
-  res.send('OK');
+app.get("/", async (req, res) => {
+  res.send("OK");
 });
 
-app.post('/event/:accountId/:mobile/:uuid', async (req, res) => {
+app.post("/event/:accountId/:mobile/:uuid", async (req, res) => {
   try {
     const {uuid, accountId, mobile} = req.params;
-    console.log('POST event', {accountId, mobile, uuid});
-    console.log('json:', req.body);
+    console.log("POST event", {accountId, mobile, uuid});
+    console.log("json:", req.body);
   } catch (e) {
-    console.log('err', e);
+    console.log("err", e);
   }
-  res.json({status: 'OK'});
+  res.json({status: "OK"});
 });
 
-app.post('/calldata/:accountId/:mobile/:uuid', async (req, res) => {
+app.post("/calldata/:accountId/:mobile/:uuid", async (req, res) => {
   try {
     const {accountId, mobile, uuid} = req.params;
-    console.log('POST calldata', {accountId, mobile, uuid});
-    console.log('json:', req.body);
+    console.log("POST calldata", {accountId, mobile, uuid});
+    console.log("json:", req.body);
   } catch (e) {
     console.log(e);
   }
-  res.json({status: 'OK'});
+  res.json({status: "OK"});
 });
 
-app.post('/record/:accountId/:mobile/:uuid',
+app.post("/record/:accountId/:mobile/:uuid",
   fileUpload({
     limits: {
       fileSize: 50 * 1024 * 1024,
@@ -44,32 +44,32 @@ app.post('/record/:accountId/:mobile/:uuid',
   }), async (req, res) => {
   try {
     const {accountId, mobile, uuid} = req.params;
-    console.log('POST record', {accountId, mobile, uuid});
+    console.log("POST record", {accountId, mobile, uuid});
 
-    const dir = path.join(config.uploadPath, '/records');
+    const dir = path.join(config.uploadPath, "/records");
     await fs.promises.mkdir(dir, {recursive: true});
-    console.log('records path', dir);
+    console.log("records path", dir);
     if (!req.files || !req.files.file) {
-      console.log('no file');
-      return res.json('no file');
+      console.log("no file");
+      return res.json("no file");
     }
     const filename = slugify(
       req.files.file.name, {
         lowercase: true,
-        separator: '_',
+        separator: "_",
       });
 
-    const fname = path.join(dir, mobile + '_' + filename);
-    console.log('prepare copy record to file', fname);
+    const fname = path.join(dir, mobile + "_" + filename);
+    console.log("prepare copy record to file", fname);
     await req.files.file.mv(fname);
-    console.log('done');
+    console.log("done");
   } catch (e) {
-    console.log('err:', e);
+    console.log("err:", e);
   }
-  res.json({status: 'OK'});
+  res.json({status: "OK"});
 });
 
-app.post('/log/:accountId/:mobile',
+app.post("/log/:accountId/:mobile",
   fileUpload({
     limits: {
       fileSize: 50 * 1024 * 1024,
@@ -77,31 +77,31 @@ app.post('/log/:accountId/:mobile',
   }), async (req, res) => {
   try {
     const {accountId, mobile} = req.params;
-    console.log('POST log:', {accountId, mobile});
+    console.log("POST log:", {accountId, mobile});
 
-    const dir = path.join(config.uploadPath, '/logs');
+    const dir = path.join(config.uploadPath, "/logs");
     await fs.promises.mkdir(dir, {recursive: true});
-    console.log('logs path', dir);
+    console.log("logs path", dir);
     if (!req.files || !req.files.file) {
-      console.log('no file');
-      return res.json('no file');
+      console.log("no file");
+      return res.json("no file");
     }
     const filename = slugify(
       req.files.file.name, {
         lowercase: true,
-        separator: '_',
+        separator: "_",
       });
 
-    const fname = path.join(dir, mobile + '_' + filename);
-    console.log('prepare copy log to file', fname);
+    const fname = path.join(dir, mobile + "_" + filename);
+    console.log("prepare copy log to file", fname);
     await req.files.file.mv(fname);
-    console.log('done');
+    console.log("done");
   } catch (e) {
-    console.log('err:', e);
+    console.log("err:", e);
   }
-  res.json({status: 'OK'});
+  res.json({status: "OK"});
 });
 
 app.listen(config.port, () => {
-  console.log('start with config', config);
+  console.log("Running on port", config.port);
 });
